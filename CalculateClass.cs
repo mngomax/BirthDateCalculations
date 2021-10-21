f5da816ed27b5d81b93d88cc692170308deadbe0
Xolani Mngoma <xolani.mngoma@derivco.co.za>
2021-10-21 16:44:42 +0200

    Add project files.

---
+++ CalculateClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthDateCalculations
+{
+    public class CalculateClass
+    {
+        public string workOutDay(string DOB= "01/04/1984")
+        {
+            int DOBLength = DOB.Length;
+            var year = DOB.Substring(DOBLength, (DOBLength - 4));
+
+            DateTime thisDay = DateTime.Today.d
+
+            string weekDay = FamilyMembers.WeekDay.Thursday.ToString();
+            return weekDay;
+        }
+    }
+}
---
+++ FamilyMembers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BirthDateCalculations
+{
+    class FamilyMembers
+    {
+        public enum WeekDay { Monday =1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+        private string _firstName;
+        public string FirstName 
+        {
+            get { return _firstName; }
+            set
+            {
+                char firstNameCase = char.Parse(value.Substring(0));
+
+                if (Char.IsUpper(firstNameCase))
+                {
+                    _firstName = value;
+                }
+                else
+                {
+                    throw new Exception("first name must contain a capital letter!");
+                }
+
+            }
+        }
+
+        public string Lastname { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        private string _email { get; set; }
+        public string email 
+        {
+            get { return _email; }
+            set
+            {
+                string pattern = null;
+                pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+
+                if (Regex.IsMatch("feedback@net-informations.com", pattern))
+                {
+                    _email = value;
+                }
+                else
+                {
+                    throw new Exception("Not a valid Email address ");
+                }
+            }
+        }
+        public FamilyMembers() 
+        {
+            //initializer
+        }
+    }
+}
