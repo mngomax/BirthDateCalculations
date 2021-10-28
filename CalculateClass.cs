93801ecd0443d0596b7c171e32b06098d7bd4013
mngomax <92936423+mngomax@users.noreply.github.com>
2021-10-28 12:09:17 +0200

    Update CalculateClass.cs

--- CalculateClass.cs
+++ CalculateClass.cs
@@ -8,8 +8,9 @@ namespace BirthDateCalculations
     {
         public string workOutDay(string DOB= "01/04/1984")
         {
-
+            
             //Make Changes 02
+            //Making wrong change
             int DOBLength = DOB.Length;
             var year = DOB.Substring(DOBLength, (DOBLength - 4));
 
